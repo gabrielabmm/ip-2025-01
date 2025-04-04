a7ee290b61477e428482fbcd9c86e1858047110b
gabriela brasileiro <gabrielabrasileiro@discente.ufg.br>
2025-04-04 16:09:49 -0300

    Add files via upload

--- lista01-go/exercicio05.go
+++ lista01-go/exercicio05.go
@@ -1,36 +1,36 @@
-programa {
-  inclua biblioteca Matematica --> mat
-  funcao inicio() {
-real contaAgua, consumo, residencial, comercial, industrial
-caracter c, C, R, r, I, i, tipo
-    escreva ("Qual o valor da conta de água? \n ")
-    leia (contaAgua)
+package main
 
-    escreva ("Qual é o consumo de água por metros cúbicos? \n")
-    leia (consumo)
+import (
+	"fmt"
+)
 
-    escreva ("Qual o tipo de consumidor? (C - Comercial, R - Residencial, I - Industrial) \n")
-    leia (tipo)
+func main() {
+	var contaAgua, consumo, residencial, comercial, industrial float64
+	var tipo string
 
-    escreva ("CONTA=", contaAgua)
-    escreva ("\n")
+	fmt.Print("Qual o valor da conta de água? \n")
+	fmt.Scan(&contaAgua)
 
-  residencial = (5 + 0.05 * contaAgua)
-  comercial = (500 + 0.25 * (contaAgua - 80))
-  industrial = (800 + 0.04 * (contaAgua - 100))
+	fmt.Print("Qual é o consumo de água por metros cúbicos? \n")
+	fmt.Scan(&consumo)
 
-  escolha (tipo)
-   {   caso "C":
-    escreva ("VALOR DA CONTA=", comercial)
-pare
-   
-   caso "R":
-    escreva ("VALOR DA CONTA =", residencial )
-   pare
+	fmt.Print("Qual o tipo de consumidor? (C - Comercial, R - Residencial, I - Industrial) \n")
+	fmt.Scan(&tipo)
 
-   caso "I":
-    escreva ("VALOR DA CONTA =", industrial)
-pare
-   }
-  }
+	fmt.Printf("CONTA= %.2f\n", contaAgua)
+
+	residencial = 5 + 0.05*contaAgua
+	comercial = 500 + 0.25*(contaAgua-80)
+	industrial = 800 + 0.04*(contaAgua-100)
+
+	switch tipo {
+	case "C", "c":
+		fmt.Printf("VALOR DA CONTA= %.2f\n", comercial)
+	case "R", "r":
+		fmt.Printf("VALOR DA CONTA= %.2f\n", residencial)
+	case "I", "i":
+		fmt.Printf("VALOR DA CONTA= %.2f\n", industrial)
+	default:
+		fmt.Println("Tipo de consumidor inválido.")
+	}
 }
