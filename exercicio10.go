90ec6016b9d0fc9af55d8be4b8634b8988ff742d
gabriela brasileiro <gabrielabrasileiro@discente.ufg.br>
2025-04-28 17:13:48 -0300

    Create exercicio10.go

---
+++ exercicio10.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+var P_inicial, D, DeltaP, P_min, i, lucro, Q_inicial, DeltaQ float64
+var preco[] float64
+var qntd[] float64
+var lucroobt[] float64
+var lucroobt2[] float64
+
+fmt.Scan(&P_inicial, &D, &DeltaP, &P_min, &Q_inicial, &DeltaQ)
+
+for i=0; i< i+1; i++{
+if (P_inicial - DeltaP * i <= P_min){
+
+preco = append(preco, (P_inicial - DeltaP * i))
+qntd = append(qntd, (Q_inicial + DeltaQ * i))
+
+	lucro = ((P_inicial - DeltaP * i) * (Q_inicial + DeltaQ * i)) - D
+
+lucroobt = append(lucroobt, lucro)
+lucroobt2 = append(lucroobt2, lucro)
+}
+
+}
+
+sort.Float64s(lucroobt2)
+
+
+vetor := []float64{lucroobt}
+
+for j:=0; j< len(vetor); j++{
+
+if (lucroobt[j] == lucroobt2[0]){
+indice := j
+}
+}
+
+fmt.Printf("%-10s %-10s %-10s\n", "Preco", "Ingressos Vendidos", "Lucro")
+
+for x, val := range preco{
+	fmt.Printf("%-10d", val)
+}
+}
