38586427fef05d2203a893b65ba794923d4b47d4
gabriela brasileiro <gabrielabrasileiro@discente.ufg.br>
2025-04-29 20:21:34 -0300

    Update exercicio10.go

--- exercicio10.go
+++ exercicio10.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-var P_inicial, D, DeltaP, P_min, i, lucro, Q_inicial, DeltaQ float64
+var P_inicial, D, DeltaP, P_min, i, lucro, Q_inicial, DeltaQ, precox, lucrox, ingressosx float64
 var preco[] float64
 var qntd[] float64
 var lucroobt[] float64
@@ -15,8 +15,7 @@ var tamanho int
 
 fmt.Scan(&P_inicial, &D, &DeltaP, &P_min, &Q_inicial, &DeltaQ)
 
-for i=0; i< i+1; i++{
-if (P_inicial - DeltaP * i <= P_min){
+for i=0; P_inicial - DeltaP*i >= P_min; i++{
 
 preco = append(preco, (P_inicial - DeltaP * i))
 qntd = append(qntd, (Q_inicial + DeltaQ * i))
@@ -26,8 +25,7 @@ qntd = append(qntd, (Q_inicial + DeltaQ * i))
 lucroobt = append(lucroobt, lucro)
 lucroobt2 = append(lucroobt2, lucro)
 
-tamanho += 1
-}
+tamanho ++
 
 }
 
@@ -36,21 +34,22 @@ sort.Float64s(lucroobt2)
 
 for j:=0; j< tamanho; j++{
 
-if (lucroobt[j] == lucroobt2[0]){
-indice := j
+if (lucroobt[j] == lucroobt2[tamanho-1]){
+precox = P_inicial - DeltaP * float64(j)
+lucrox = ((P_inicial - DeltaP * float64(j)) * (Q_inicial + DeltaQ * float64(j))) - D
+ingressosx = Q_inicial + DeltaQ * float64(j)
+break
 }
 }
 
-fmt.Printf("%-10s %-10s %-10s\n", "Preco", "Ingressos Vendidos", "Lucro")
 
-for i=0; i< tamanho; i++{
-	fmt.Printf("%-10d \n", preco[i])}
+fmt.Printf("%-20s %-20s %-20s\n", "Preco", "Ingressos Vendidos", "Lucro")
 
-	for i=0; i< tamanho; i++{
-		fmt.Printf("%-10d \n", qntd[i])}
+for q := 0; q < tamanho; q++ {
+	fmt.Printf("%-20.2f %-20.2f %-20.2f\n", preco[q], qntd[q], lucroobt[q])
+}
 
-		for i=0; i< tamanho; i++{
-			fmt.Printf("%-10d \n", lucroobt[i])}
+fmt.Println("\nMelhor opção:")
+fmt.Printf("Preço: %.2f, Ingressos vendidos: %.2f, Lucro: %.2f\n", precox, ingressosx, lucrox)
 			
-			fmt.Printf("Preco min %-10d" preco[indice])
 }
